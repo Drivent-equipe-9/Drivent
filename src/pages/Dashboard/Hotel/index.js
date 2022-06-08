7ed7ff2a85adf2cbcc5dd25de9f80b32bac995c6
Lucas <lucasazzollinivieira@gmail.com>
2022-06-07 21:51:40 -0300

    feat: warning pages
    Co-authored-by: Bruna Souza <busouza01@gmail.com>

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,77 @@
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { StyledTypography } from '../../../components/PersonalInformationForm';
+import useToken from '../../../hooks/useToken';
+import { getPersonalInformations } from '../../../services/enrollmentApi';
+import { findPayment, findTicket } from '../../../services/ticketApi';
+import { ContainerEmptyInfo, EmptyInfoText } from '../Payment/style';
+
 export default function Hotel() {
-  return 'Hotel: Em breve!';
+  const token = useToken();
+  const [isPaid, setPaid] = useState(false);
+  const [hasAccomodation, setAccomodation] = useState(false);
+
+  useEffect(() => {
+    const promiseEnrollment = getPersonalInformations(token);
+    promiseEnrollment
+      .then((response) => {
+        const enrollmentData = response.id;
+
+        const promiseTicket = findTicket(token, enrollmentData);
+        promiseTicket
+          .then((response) => {
+            if (response.withAccommodation) {
+              setAccomodation(true);
+            } else {
+              setAccomodation(false);
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch(() => {
+        toast.error('Inscrição não encontrada');
+      });
+
+    const promisePayment = findPayment(token);
+    promisePayment
+      .then((response) => {
+        if (response.isPaid) {
+          setPaid(true);
+        } else {
+          setPaid(false);
+          toast('Pagamento não realizado.');
+        }
+      })
+      .catch(() => {
+        toast.error('Pagamento não encontrado.');
+      });
+  }, []);
+
+  return (
+    <>
+      <StyledTypography variant='h4'>Escolha de hotel e quarto</StyledTypography>
+      {!isPaid ?
+        <ContainerEmptyInfo>
+          <EmptyInfoText>
+            Você precisa ter confirmado pagamento antes <br />
+            de fazer a escolha de hospedagem
+          </EmptyInfoText>
+        </ContainerEmptyInfo>
+        :
+        ''
+      }
+      {!hasAccomodation ?
+        <ContainerEmptyInfo>
+          <EmptyInfoText>
+            Sua modalidade de ingresso não inclui hospedagem <br />
+            Prossiga para a escolha de atividades
+          </EmptyInfoText>
+        </ContainerEmptyInfo>
+        :
+        ''
+      }
+    </>
+  );
 }
