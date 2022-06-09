196e073eb280c748d860c50323acdba8a45179e6
luis garbelotti <luisgsgarbelotti@gmail.com>
2022-06-09 19:18:42 -0300

    chore: create requisitions to get Hotel infos and Rooms infos when given hotel id
    Co-authored-by: Carol <caroline.y.ldm@gmail.com>
    
    Add GET method requisition to get hotel infos (route '/hotels) and Rooms infos ('/hotels/:hotelId/rooms') by hotel id

---
+++ src/services/hotelApi.js
@@ -0,0 +1,39 @@
+import api from './api';
+
+export async function getHotelInfo(token) {
+  const response = await api.get('/hotels', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+}
+
+export async function getRooms(hotelId, token) {
+  const response = await api.get(`/hotels/${hotelId}/rooms`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+}
+
+export async function saveHotel(token, body) {
+  const response = await api.post('/hotel', body, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
+
+export async function saveRoom(token, body) {
+  const response = await api.post('/hotel/room', body, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
