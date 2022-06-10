3a8b7142016d9f6c40fd739edd39a5f8faed276d
luis garbelotti <luisgsgarbelotti@gmail.com>
2022-06-09 21:04:58 -0300

    chore: render vacancies left
    Co-authored-by: Bruna <busouza01@gmail.com>
    Co-authored-by: Carol <caroline.y.ldm@gmail.com>
    Co-authored-by: Lucas <lucasazzollinivieira@gmail.com>
    
    Change to render the correct vacancies left for each hotel and remove some useless states

--- src/pages/Dashboard/Hotel/hotelModality/hotelModality.js
+++ src/pages/Dashboard/Hotel/hotelModality/hotelModality.js
@@ -5,17 +5,15 @@ import useToken from '../../../../hooks/useToken';
 import { getRooms } from '../../../../services/hotelApi';
 import { useState } from 'react';
 
-export function HotelModality({ hotelInfo }) {
+export function HotelModality({ hotelInfo, setIsSelected }) {
   const token = useToken();
-  const [selectedHotel, setSelectedHotel] = useState();
-  const [isSelected, setIsSelected] = useState(false);
   const [rooms, setRooms] = useState([]);
   const [formReservationData, setFormReservationData] = useState({
     roomId: '',
     hotelId: '',
   });
   
-  function handleHotelChange(e, h) {
+  function handleHotelChange(h) {
     renderHotel(h);
     setFormReservationData({ ...formReservationData, hotelId: h.id });
     const promiseRoom = getRooms(h.id, token);
@@ -32,8 +30,10 @@ export function HotelModality({ hotelInfo }) {
     for (let i = 0; i < hotelInfo.length; i++) {
       if( i === hotels.id - 1) {
         hotelInfo[i].isSelected = true;
+        setIsSelected(true);
       } else {
         hotelInfo[i].isSelected = false;
+        setIsSelected(true);
       }
     }
 
@@ -46,13 +46,17 @@ export function HotelModality({ hotelInfo }) {
 
       <Container>
         {hotelInfo?.map((h) => (
-          <Option key={h.id} id={h.id} name={h.name} onClick={(e) => handleHotelChange(e, h)} isSelected={h.isSelected}>
+          <Option key={h.id} id={h.id} name={h.name} onClick={() => handleHotelChange(h)} isSelected={h.isSelected}>
             <img src={h.imageUrl} alt={h.name}></img>
             <h2>{h.name}</h2>
             <h3>Tipos de acomodação:</h3>
             <p>{h.accomodationType}</p>
             <h3>Vagas disponíveis:</h3>
-            <p>0</p>
+            {!h?.vacanciesLeft ? 
+              <p>0</p>
+              :
+              <p>{h.vacanciesLeft}</p>
+            }
           </Option>
         ))}
       </Container>
